65881558df419a8f6cfe19fd0a97b4d565de6abe
Umar Sunusi Maitalata <maitalata@gmail.com>
2022-01-01 18:49:39 +0100

    Scaffolding

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="app.css">
+  </head>
+  <body>
+    <article class="">
+    
+        <?= $post ?>
+
+    </article>
+
+    <a href="/"> Go back</a>
+
+  </body>
+</html>
