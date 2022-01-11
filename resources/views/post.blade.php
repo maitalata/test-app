b47a8705a122bef8bb06f615e8311564daaa3858
Umar Sunusi Maitalata <maitalata@gmail.com>
2022-01-11 10:01:12 +0100

    Added more blade to views

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="../app.css">
-  </head>
-  <body>
-    <article class="">
+@extends ('layout')
 
-       <h1>{{$post->title}}</h1>
-       <div>
-           {!!$post->body!!}
-       </div>
-
-    </article>
-
-    <a href="/"> Go back</a>
-
-  </body>
-</html>
+@section('content')
+    @foreach ($posts as $post)
+        <article class="">
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    <!-- <?= $post->title ?> -->
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+@endsection
