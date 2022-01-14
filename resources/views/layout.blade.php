68583d4de24da03fee5829d71c974af1a6f80f2b
Umar Sunusi Maitalata <maitalata@gmail.com>
2022-01-14 17:36:09 +0100

    Little modifications

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>My Blog</title>
-    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="/app.css">
   </head>
   <body>
     @yield('content')
