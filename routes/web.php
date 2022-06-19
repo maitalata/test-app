c467e2d832676f53548cad481af8205a280f68b6
Umar Sunusi Maitalata <maitalata@gmail.com>
2022-06-19 19:46:43 +0100

    Added search capability, messy way though

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,16 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest();
+
+    if (request('search')) {
+        $posts->where('title', 'like', '%'.request('search').'%')
+            ->orWhere('body', 'like', '%'.request('search').'%');
+
+    }
+
     return view('posts', [
-        'posts' => Post::with('category', 'author')->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
 });
@@ -44,4 +52,3 @@ Route::get('authors/{author:username}', function(User $author){
         'posts' => $author->posts->load('category', 'author')
     ]);
 });
-
